c4ed3613a693fc7f907fbc2f5c461b1dcca9b6e8
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-10-06 16:09:31 +0300

    feat: project structure

---
+++ XmlProcessingSystem/FileParserService/Program.cs
@@ -0,0 +1,53 @@
+﻿using FileParserService.Services;
+using FileParserService.Services.Configurations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using XmlProcessingSystem.Shared.Services;
+using XmlProcessingSystem.Shared.Services.Configurations;
+
+namespace FileParserService;
+
+public sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddEnvironmentVariables()
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        var fileMonitorSettings = new FileMonitorConfiguration()
+        {
+            MonitoringDelayMiliseconds = int.Parse(config["WatchDelayMiliseconds"]),
+            WatchingDirectory = config["Monitor:WatchDirectory"]
+        };
+
+        var rabbitMQSettings = new RabbitMQSettings
+        {
+            Host = config["RABBITMQ_HOST"],
+            UserName = config["RABBITMQ_USER"],
+            Password = config["RABBITMQ_PASSWORD"],
+            Port = int.Parse(config["RABBITMQ_PORT"]),
+            QueueName = config["RABBITMQ_QUEUE"]
+        };
+
+        using var loggerFactory = LoggerFactory.Create(builder => 
+        {
+
+            builder.AddConsole()
+                .SetMinimumLevel(LogLevel.Information);
+        });
+
+        var messageBuss = new RabbitMQMessageBus()
+
+        var sender = new Sender()
+
+        var xmlProcessor = new XmlProcesser()
+
+        var fileMonitor = new FileMonitor(
+            fileMonitorSettings,
+            
+        )
+    }
+}
